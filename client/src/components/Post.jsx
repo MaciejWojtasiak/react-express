36699867ef47946da7f3dfd1a30d70348abde6b8
Maciej Wojtasiak <maciekwojtasiak@gmail.com>
2023-01-15 11:47:58 +0100

    created routes and models

---
+++ client/src/components/Post.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Post({name, text, like}) {
+  return (
+    <div></div>
+  )
+}
+
+export default Post
--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+require("dotenv").config({ path: "./config.env" });
 const PORT = 3000;
+const authRoute = require('./routes/auth');
+app.use(cors({ origin: "http://localhost:5173" }))
+app.use(express.json())
 
-app.use(cors);
 
+mongoose.set('strictQuery', false);
+mongoose
+    .connect(process.env.ATLAS_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server listens on port ${PORT}`)
+        });
+    })
 
-app.get('/', ((req, res) => {
-    res.send('Server running.')
-}));
-
-app.listen(PORT, () => {
-    console.log(`Server listens on port ${PORT}`)
-})
-
+app.use('/auth', authRoute)
