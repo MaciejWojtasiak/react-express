f3ee17283bd476bbbe6f8d6102b4cf1d8d4392ca
Maciej Wojtasiak <maciekwojtasiak@gmail.com>
2023-02-03 19:57:11 +0100

    Post title and description editing

--- client/src/components/SinglePost/SinglePost.jsx
+++ client/src/components/SinglePost/SinglePost.jsx
@@ -10,6 +10,9 @@ function SinglePost() {
   const location = useLocation();  
   const postID = location.pathname.replace('/post/','');  
   const [post, setPost] = useState(false);
+  const [edit, setEdit] = useState(false);
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
   const {user} = useContext(Context); 
 
   useEffect(()=>{
@@ -17,6 +20,8 @@ function SinglePost() {
       const response = await fetch(`http://localhost:3000/posts/${postID}`);
       const data = await response.json();
       setPost(data);
+      setTitle(data.title);
+      setDesc(data.desc);
     }
     fetchPost();
   },[]);
@@ -39,14 +44,39 @@ function SinglePost() {
       console.log(err);
     };    
   }
+  const editPost = () => {
+    setEdit(true);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`http://localhost:3000/posts/${postID}`, {
+      method:"PUT",
+      mode:'cors',
+      headers: {
+        Accept: 'application.json',
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify({title:title,desc:desc, username:user.username}),
+      cache: 'default'
+    });
+    console.log(response)    
+    response.ok && window.location.reload();
+    } catch (err) {
+      console.log(err)
+    }    
+  }
+  
+
   return (
     <div className='singlePost'>
       <div className="singlePostWrapper">
         {post.photo && (<img className='singlePostImage' src={PF + post.photo} alt="post_image" />)}
         <h1 className='singlePostTitle'>
-          {post.title}
+          {edit ? <input type="text" className="titleInput" value={title} onChange={e=>setTitle(e.target.value)}></input> : post.title}       
           <div className="singlePostEdit">
-            <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
+            <i className="singlePostIcon fa-solid fa-pen-to-square" onClick={editPost}></i>
             <i className="singlePostIcon fa-solid fa-trash" onClick={deletePost}></i>
           </div>
         </h1>
@@ -55,8 +85,9 @@ function SinglePost() {
           <span className='singlePostDate'>{new Date(post.createdAt).toDateString() }</span>
         </div>
         <p className='singePostDescription'>
-          {post.desc}
+          {edit ? <textarea type="text" className="descInput" value={desc} onChange={e=>setDesc(e.target.value)}></textarea> : post.desc}  
         </p>
+        {edit && <button className='editBtn' type='submit' onClick={handleSubmit}>Edit</button>}
       </div>
     </div>
   )
